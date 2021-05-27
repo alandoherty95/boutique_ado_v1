6ef8af778247f38f1b2d6a6ca41712543811e3a7
alandoherty95 <alandoherty95@gmail.com>
2021-05-27 12:44:19 +0000

    Added friendly name for Categories

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
+SECRET_KEY = 'django-insecure-vkkqc9ko#2x4*hke4hugir=&p9i^=8mcii@vpz&u3wlndigi0w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
